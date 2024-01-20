f084ccd8ded120ebcab2954ced3e67701b25c5b2
pratham-ak2004 <pratham20442@gmail.com>
2024-01-20 23:28:22 +0530

    bug fix : FriendList.jsx

--- src/components/friends/FriendList.jsx
+++ src/components/friends/FriendList.jsx
@@ -31,62 +31,19 @@ export default function FriendList() {
 
   return (
     <>
-      <div className={`h-auto min-h-screen overflow-y-auto p-2 md:max-w-lg bg-slate-300 w-full md:basis-1/3 ${location.pathname.startsWith("/chat/")&&(!location.pathname.endsWith("/chat/"))&&isMobile ? "hidden" : ""}`}>
+      <div className={`h-auto min-h-screen overflow-y-auto p-2 bg-slate-300 w-full md:basis-1/3 ${location.pathname.startsWith("/chat/")&&(!location.pathname.endsWith("/chat/"))&&isMobile ? "hidden" : ""}`}>
         <div className="gap-3 grid items-start overflow-y-auto pb-20 overflow-x-hidden">
 
         <div className="bg-slate-100 w-full h-24 rounded-lg shadow-xl flex flex-col items-center justify-center font-extrabold text-2xl text-slate-700" onClick={handleAddFriend}> + Add Friend</div>
         
-        <div className="flex flex-row items-center bg-slate-100 shadow-xl rounded-lg p-4">
+        <div className="flex flex-row items-center bg-slate-100 shadow-xl rounded-lg p-4" onClick={handleChatSelect}>
           <img className="w-16 h-16 rounded-full" src="https://via.placeholder.com/150" alt="Profile" />
-          <div className="ml-3" onClick={handleChatSelect}>
+          <div className="ml-3">
             <h2 className="text-2xl font-medium text-left tracking-wide text-nowrap my-1">Lorem ipsum</h2>
             <p className="font-thin tracking-tight text-nowrap text-left">Lorem ipsum dolor sit amet consectetur.</p>
           </div>
         </div>
 
-        <div className="flex flex-row items-center bg-slate-100 shadow-xl rounded-lg p-4">
-          <img className="w-16 h-16 rounded-full" src="https://via.placeholder.com/150" alt="Profile" />
-          <div className="ml-3" onClick={handleChatSelect}>
-            <h2 className="text-2xl font-medium text-left tracking-wide text-nowrap my-1">Lorem ipsum</h2>
-            <p className="font-thin tracking-tight text-nowrap text-left">Lorem ipsum dolor sit amet consectetur.</p>
-          </div>
-        </div>
-        <div className="flex flex-row items-center bg-slate-100 shadow-xl rounded-lg p-4">
-          <img className="w-16 h-16 rounded-full" src="https://via.placeholder.com/150" alt="Profile" />
-          <div className="ml-3" onClick={handleChatSelect}>
-            <h2 className="text-2xl font-medium text-left tracking-wide text-nowrap my-1">Lorem ipsum</h2>
-            <p className="font-thin tracking-tight text-nowrap text-left">Lorem ipsum dolor sit amet consectetur.</p>
-          </div>
-        </div>
-        <div className="flex flex-row items-center bg-slate-100 shadow-xl rounded-lg p-4">
-          <img className="w-16 h-16 rounded-full" src="https://via.placeholder.com/150" alt="Profile" />
-          <div className="ml-3" onClick={handleChatSelect}>
-            <h2 className="text-2xl font-medium text-left tracking-wide text-nowrap my-1">Lorem ipsum</h2>
-            <p className="font-thin tracking-tight text-nowrap text-left">Lorem ipsum dolor sit amet consectetur.</p>
-          </div>
-        </div>
-        <div className="flex flex-row items-center bg-slate-100 shadow-xl rounded-lg p-4">
-          <img className="w-16 h-16 rounded-full" src="https://via.placeholder.com/150" alt="Profile" />
-          <div className="ml-3" onClick={handleChatSelect}>
-            <h2 className="text-2xl font-medium text-left tracking-wide text-nowrap my-1">Lorem ipsum</h2>
-            <p className="font-thin tracking-tight text-nowrap text-left">Lorem ipsum dolor sit amet consectetur.</p>
-          </div>
-        </div>
-        <div className="flex flex-row items-center bg-slate-100 shadow-xl rounded-lg p-4">
-          <img className="w-16 h-16 rounded-full" src="https://via.placeholder.com/150" alt="Profile" />
-          <div className="ml-3" onClick={handleChatSelect}>
-            <h2 className="text-2xl font-medium text-left tracking-wide text-nowrap my-1">Lorem ipsum</h2>
-            <p className="font-thin tracking-tight text-nowrap text-left">Lorem ipsum dolor sit amet consectetur.</p>
-          </div>
-        </div>
-        <div className="flex flex-row items-center bg-slate-100 shadow-xl rounded-lg p-4">
-          <img className="w-16 h-16 rounded-full" src="https://via.placeholder.com/150" alt="Profile" />
-          <div className="ml-3" onClick={handleChatSelect}>
-            <h2 className="text-2xl font-medium text-left tracking-wide text-nowrap my-1">Lorem ipsum</h2>
-            <p className="font-thin tracking-tight text-nowrap text-left">Lorem ipsum dolor sit amet consectetur.</p>
-          </div>
-        </div>
-      
         </div>
       </div>
     </>
